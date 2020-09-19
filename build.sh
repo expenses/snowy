79c40702e348a20114f6c7957f6db31dda8a6edf
Ashley Ruglys <ashley.ruglys@gmail.com>
2020-09-19 23:03:24 +0200

    TEST

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --target wasm32-unknown-unknown --no-default-features --features wasm --release
+wasm-bindgen --out-dir pkg --web ../target/wasm32-unknown-unknown/release/snowy.wasm
+python -m http.server 8000
+
